0dfc93fe35ce6ad4a7b4727cc339a7f5a3e8676f
CN24 <codenation@CN24s-MacBook-Pro.local>
2020-02-24 13:24:36 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    list: [],
+    currentInput: "",
+  }
+
+  addHandler = e => {
+    this.setState({ currentInput: e.target.value })
+  }
+
+  submit = () => {
+    let storeInput = this.state.list
+    if(storeInput === null || storeInput === ""){
+      alert("Please Type In The Space Below")
+      return false
+    }
+    storeInput.push(this.state.currentInput)
+    this.setState({list: storeInput, currentInput: ""})
+    console.log("Hi o/")
+  }
+
+  removeHandler = index => {
+    let storeList = this.state.list
+    storeList.splice(index, 1)
+    this.setState({ list: storeList })
+  }
+
+  enterHandler = (event) => {
+    if (event.key === 'Enter'){
+      this.submit()
+    }
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="title">
+          <h1>To-Do-List</h1>
+        </div>
+        <div className="to-do-list">
+        <button className="button1" onClick={this.submit}>Add</button>
+        <input className="text" type="text"  placeholder="Type Here" value={this.state.currentInput} onChange={this.addHandler} onKeyPress={this.enterHandler}/>
+        {this.state.list.map((savedInput, index) => {
+          return (
+            <div className="list">
+              <button className="button2" key={index} onClick={() => this.removeHandler(index)}/>
+              <p>{savedInput}</p>
+            </div>
+          )
+        })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
