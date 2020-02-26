d6a89903addfab289007fbca6774903d21c84c71
Ga Ming Chan <gmchan19@gmail.com>
2020-02-26 16:43:50 +0000

    Updates

---
+++ precache-manifest.56229ae00aba957d2aed52b24ef2db98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1154bc44f2f5b0d487a1ef46848a73",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "7fe6b1f5421786ff5d0f",
+    "url": "/to-do-list/static/css/main.9fa00a98.chunk.css"
+  },
+  {
+    "revision": "e2af6c8abd3b08e65530",
+    "url": "/to-do-list/static/js/2.0fc344b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/to-do-list/static/js/2.0fc344b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe6b1f5421786ff5d0f",
+    "url": "/to-do-list/static/js/main.a69e60a6.chunk.js"
+  },
+  {
+    "revision": "ef1ab25b4db95c178911",
+    "url": "/to-do-list/static/js/runtime-main.7557c059.js"
+  }
+]);
